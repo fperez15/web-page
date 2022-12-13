6a0a1619a075d8c6f8f24e02d65269e0d4740bc4
fperez15 <francis.perez1590@gmail.com>
2022-12-13 12:35:18 -0500

    footer

--- js/questions.js
+++ js/questions.js
@@ -6,6 +6,11 @@
         ()=> {
            let height = 0; // se calcula el alto del elemento
            let answer = questions.nextElementSibling; // se obtiene el hermano de .questions__title que es .questions__show
+           let addPadding = questions.parentElement.parentElement; // se obtiene el padre de la clase .questions__title que es .questions anwser y a su vez el padre de esta clase q es questions__padding, la cual es la clase q necesitamos (.questions__padding).
+           
+           addPadding.classList.toggle('questions__padding--add'); // se obtienen las clases de addPadding 
+           questions.children[0].classList.toggle('questions__arrow--rotate');  // hace rotar la flecha... .toggle() permite alternar la visibilidad de un elemento.
+
 
            if (answer.clientHeight === 0) {
             height = answer.scrollHeight; // alto minimo para q un elemento se me muestre
